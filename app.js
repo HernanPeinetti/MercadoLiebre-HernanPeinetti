6c610c8873f229ed5621a839f1c2893c2c3d2526
Hernan <hernan.peinetti18@gmail.com>
2023-10-23 23:11:11 -0300

    Creacion de estructura HTML

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+let pathPublic = path.resolve("public");
+app.use(express.static(pathPublic));
+
+app.listen(3000, () =>{
+    console.log("Servidor Levantado");
+})
+
+app.get("/", (req, res)=>{
+    let homeHtml = path.resolve(__dirname, "./views/home.html")
+    res.sendFile(homeHtml);
+})
+
