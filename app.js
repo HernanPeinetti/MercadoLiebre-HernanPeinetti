0158d4544bb5ef98dbd2243c0ef55004877130b6
Hernan <hernan.peinetti18@gmail.com>
2023-11-09 01:27:19 -0300

    login casi terminado

--- app.js
+++ app.js
@@ -5,12 +5,29 @@ const app = express();
 let pathPublic = path.resolve("public");
 app.use(express.static(pathPublic));
 
+ let pathPublic2 = path.resolve("views");
+ app.use(express.static(pathPublic2));
+
+
+
 app.listen(3000, () =>{
     console.log("Servidor Levantado");
 })
 
 app.get("/", (req, res)=>{
-    let homeHtml = path.resolve(__dirname, "./views/home.html")
+    let homeHtml = path.resolve(__dirname, "./views/home.html");
     res.sendFile(homeHtml);
-})
+
+});
+
+app.get("/login", (req, res) =>{
+    let loginHtml = path.resolve(__dirname, "./views/login.html");
+    res.sendFile(loginHtml);
+});
+
+
+
+    
+
+
 
